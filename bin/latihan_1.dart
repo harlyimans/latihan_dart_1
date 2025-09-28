e8682eeacc6ebeaffe41477c50c480670e19082e
harlyimans <119984227+harlyimans@users.noreply.github.com>
2025-09-28 09:27:21 +0700

    Edit Code variabel

--- bin/latihan_1.dart
+++ bin/latihan_1.dart
@@ -4,6 +4,17 @@
 //   print('Hello world: ${latihan_1.calculate()}!');
 // }
 
+// void main(List<String> arguments) {
+//   print('Hello world: Selamat Pagi !');
+// }
 void main(List<String> arguments) {
-  print('Hello world: Selamat Pagi !');
+  var fullname = 'Dart Programming';
+  var age = 1;
+  var height = 1.5;
+  var ismarried = true;
+
+  print(fullname);
+  print(age);
+  print(height);
+  print(ismarried);
 }
