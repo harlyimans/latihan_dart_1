befe0d0589993c7051953d5605524082cbf2d6b2
harlyimans <119984227+harlyimans@users.noreply.github.com>
2025-09-28 09:06:58 +0700

    remove library, update main hello world

--- bin/latihan_1.dart
+++ bin/latihan_1.dart
@@ -1,5 +1,9 @@
-import 'package:latihan_1/latihan_1.dart' as latihan_1;
+// import 'package:latihan_1/latihan_1.dart' as latihan_1;
+
+// void main(List<String> arguments) {
+//   print('Hello world: ${latihan_1.calculate()}!');
+// }
 
 void main(List<String> arguments) {
-  print('Hello world: ${latihan_1.calculate()}!');
+  print('Hello world: Selamat Pagi !');
 }
